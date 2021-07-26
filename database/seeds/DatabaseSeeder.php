a3fbb8d4aff5f4e071de95e0a8779378addc2651
planeset94 <planeset94@gmail.com>
2021-07-26 20:03:18 +0200

    add CategorySeeder to DatabaseSeeder for automatic seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([ArticleSeeder::class]);
+        $this->call(
+            [
+                ArticleSeeder::class,
+                CategorySeeder::class          
+            ]
+        );
     }
 
 }
